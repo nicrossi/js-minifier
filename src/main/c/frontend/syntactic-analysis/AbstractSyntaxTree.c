6d2a14888a4fd8acb387945c2f4108ea3d851c5c
Nico Rossi <nrossi@itba.edu.ar>
2025-06-20 00:25:47 -0300

    handle release integer expression, for consistency

--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
@@ -89,6 +89,9 @@ void releaseExpression(Expression * expression) { //NOLINT
                     expression->callExpression = NULL;
                 }
                 break;
+            case INTEGER_EXPRESSION:
+                // No need to release anything, as the integer is a primitive type.
+                break;
             default:
                 logWarning(_logger, "Unknown expression type: %d", expression->type);
         }
